f3e44ff959e20707659edb34c8ef9ca9c83ffcc1
kwalter <walterkaunda@live.co.uk>
2019-06-20 15:32:52 +0200

    Added filtering of villages by district id.

--- app/controllers/api/v1/villages_controller.rb
+++ app/controllers/api/v1/villages_controller.rb
@@ -11,13 +11,14 @@ class Api::V1::VillagesController < ApplicationController
   end
 
   def index
-    filters = params.permit(%i[traditional_authority_id name])
+    filters = params.permit(%i[traditional_authority_id name district_id])
 
     if filters.empty?
       render json: paginate(Village.order(:name))
     else
       inexact_filters = make_inexact_filters(filters, [:name])
-      render json: paginate(Village.where(*inexact_filters).order(:name))
+      villages = Village.joins(:traditional_authority).where(*inexact_filters).order(:name)
+      render json: paginate(villages)
     end
   end
 end
