60ec4ee8357bfb870237244bc22a2afe2a8b2207
Walter Kaunda <walterkaunda@live.co.uk>
2020-10-16 13:00:02 +0200

    Added GET /patients/:patient_id request tests

--- spec/requests/patient_spec.rb
+++ spec/requests/patient_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'rails_helper'
+
 RSpec.describe '/patients', type: :request do
   describe 'GET /patients' do
     let(:person) { create(:person) }
@@ -17,6 +19,43 @@ RSpec.describe '/patients', type: :request do
         expect(json.size).to eq(1)
         expect(json.first['patient_id']).to eq(patient.id)
       end
+    end
+  end
+
+  describe 'GET /patients/:patient_id' do
+    let(:person) { create(:person) }
+    let(:program) { create(:program) }
+    let(:service) { PatientService.new }
+    let(:national_identifier_type) { PatientIdentifierType.find_by!(name: 'National ID') }
+
+    it 'returns an existing patient by id' do
+      patient = service.create_patient(program, person)
+
+      get api_route("patients/#{patient.patient_id}")
+
+      expect(response).to have_http_status(:ok)
+
+      parse_response(response) do |json|
+        expect(json['patient_id']).to eq(patient.patient_id)
+
+        identifiers = json['patient_identifiers'].each_with_object([]) do |identifier, array|
+          next unless identifier['identifier_type'] == national_identifier_type.id
+
+          array << identifier['identifier']
+        end
+
+        expect(identifiers.size).to eq(1)
+        expect(identifiers.first).not_to be_nil
+      end
+    end
+
+    it 'raises 404 for non-existent patients' do
+      patient = service.create_patient(program, person)
+      patient.void("I don't like this guy")
+
+      get api_route("patients/#{patient.patient_id}")
+
+      expect(response).to have_http_status(404)
     end
   end
 end
